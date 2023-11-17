1901ec41d8b2824215b9d14eb6d3d930c3c5dcbe
Jacqueline Xu <xujacque@gmail.com>
2023-11-16 20:42:20 -0500

    create questions and multiple choices

---
+++ qna.py
@@ -0,0 +1,144 @@
+Questions = {
+    "Traditionally, what kind of icing frosts a carrot cake?": [
+        "Butter Cream",
+        "Whipped Cream",
+        "Ganache",
+        "Cream cheese"
+    ],
+    "Which ingredients combine to create a ganache?": [
+        "Whipping cream and melted chocolate",
+        "Cream cheese and melted chocolate",
+        "Vanilla extract and peanut butter",
+        "Ladyfinger cookies and coffee"
+    ],
+    "Black Forest cake originated in which country?": [
+        "Germany",
+        "France",
+        "Italy",
+        "Switzerland"
+    ],
+    "Other than butter, what is the main ingredient in buttercream frosting?": [
+        "Powdered sugar",
+        "Butternut squash",
+        "Egg whites",
+         "Water"
+    ],
+    "In a New Orleans classic, bread pudding, what liquor is poured on top?": [
+        "Vodka",
+        "Tequila",
+        "Bourbon",
+        "Rum"
+    ],
+    "What are the three components in the fancy dessert, baked Alaska?": [
+        "Pound cake, meringue, and ice cream",
+        "Carrot cake, meringue, and vanilla extract",
+        "Sponge cake, chocolate creme, and whipped cream",
+        "Sponge cake, meringue, and ice cream"
+    ],
+    "A turtle candy is commonly made out of which three ingredients?": [
+        "Vanilla, butter, and almonds",
+        "Nutmeg, cinnamon, and sugar",
+        "Caramel, chocolate, and pecans",
+        "Caramel, chocolate, and almonds"
+    ],
+    "What was the profession of William Morrison, the man who invented machine-spun cotton candy?": [
+        "Writer",
+        "Teacher",
+        "Scientist",
+        "Dentist"
+    ],
+    "Which dessert is named after a famous ballerina?": [
+        "Baked Alaska",
+        "Banana split",
+        "Angel food cake",
+        "Raspberry Pavlova"
+    ],
+    "What is meant to be the main flavor in a traditional tiramisu?": [
+        "Coffee",
+        "Matcha",
+        "Vanilla",
+        "Strawberry"
+    ],
+    "Which popular Greek dessert is made from filo pastry?": [
+        "Cannoli",
+        "Baklava",
+        "Biscotti",
+        "Tiramisu"
+    ],
+    "The name for this popular dessert stems from the French word for “froth” or “foam”.": [
+        "Creme brulee",
+        "Macaroon",
+        "Mousse",
+        "Creme fraiche"
+    ],
+    "Which cake is known as the oldest cake in existence?": [
+        "Pound Cake",
+        "Angel Food Cake",
+        "Devil's Food Cake",
+        "Linzer Torte"
+    ],
+    "What are the four main ingredients in ice cream?": [
+        "Eggs, milk, cream, and sugar",
+        "Flour, sugar, egg whites, water",
+        "Vanilla extract, riceflour, salt, and sugar",
+        "Flour, milk, salt, and sugar"
+    ],
+    "What was the first JELL-O pudding flavor made in 1936?": [
+        "Vanilla",
+        "Strawberry",
+        "Banana",
+        "Chocolate"
+    ],
+    "Which distinct ingredient is used in Italian cheesecakes?": [
+        "Ricotta cheese",
+        "Cream cheese",
+        "Mascarpone cheese",
+        "Cottage cheese"
+    ],
+    "A common cookie made around Christmas time includes the ingredients of egg whites and coconut. What is the name of this cookie?": [
+        "Coconut cream pie",
+        "Coconut custard",
+        "Coconut macaroon",
+        "Coconut cake"
+    ],
+    "Percentage-wise, how much of a Twinkie consists of air?": [
+        "68%",
+        "23%",
+        "5%",
+        "94%"
+    ], 
+    "What was the name of the eleven-year-old boy that created popsicles?": [
+        "Frank Epperson",
+        "Arthur Granville",
+        "Sammy Freeze",
+        "Peter Chillingsworth",
+    ],
+    "Which candy bar is named after the Mars’ family horse?": [
+        ""
+        "Snickers",
+
+    ],
+    "Marzipan is made from which nuts?": [
+        "Pistachios", 
+        "Almonds", 
+        "Walnuts", 
+        "Hazelnuts"
+    ],
+    "Soda mixed with ice cream creates which dessert?": [
+        "Exploding Froth"
+        "Creamy Fizz"
+        "A float"
+        "Frothy Delight"
+    ],
+    "What is the best-selling packaged cookie in the United States?": [
+        "Chips Ahoy!",
+        "Keebler Elf Fudge",
+        "Nutter Butter",
+        "Oreo’s"
+    ],
+    "In which state was the chocolate chip cookie invented?": [
+        "New York",
+        "Pennsylvania",
+        "California",
+        "Massachusetts"]
+}
